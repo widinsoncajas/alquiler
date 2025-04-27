5cdd4ad638f797ba76105789995972acc28c3aec
widinsoncajas <adriancajas2002@gmail.com>
2025-04-27 00:51:14 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const app = express();
 
 // Ruta principal para levantar servidor PHP
 app.get('/', (req, res) => {
-  exec('php -S 0.0.0.0:8080 -t ' + path.join(__dirname, 'GAMA_FAMILIAR'), (err, stdout, stderr) => {
+  exec('php -S 0.0.0.0:8082 -t ' + path.join(__dirname, 'GAMA_FAMILIAR'), (err, stdout, stderr) => {
     if (err) {
       console.error('Error ejecutando el servidor PHP:', err);
       return res.status(500).send('Error ejecutando el servidor PHP: ' + err.message);
@@ -14,7 +14,7 @@ app.get('/', (req, res) => {
       console.error('stderr:', stderr);
       // A veces PHP lanza stderr como advertencia, pero igual funciona
     }
-    res.send('Servidor PHP corriendo en http://localhost:8080/GAMA_FAMILIAR.php');
+    res.send('Servidor PHP corriendo en http://localhost:8082/GAMA_FAMILIAR.php');
   });
 });
 
