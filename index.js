3b36bcf60ef3352292526c56cd1c2a3d013545b5
widinsoncajas <adriancajas2002@gmail.com>
2025-04-26 23:40:31 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
-<<<<<<< HEAD
 const express = require('express');
 const path = require('path');
 const { exec } = require('child_process'); // Para ejecutar comandos del sistema
 const app = express();
 
-// Ruta para ejecutar y servir el archivo PHP
+// Ruta para ejecutar y servir el archivo PHP principal
 app.get('/', (req, res) => {
   // Comando para ejecutar el archivo PHP
   exec('php ' + path.join(__dirname, 'inicio.php'), (err, stdout, stderr) => {
@@ -23,9 +22,10 @@ app.get('/', (req, res) => {
   });
 });
 
-// Configurar express para servir archivos estáticos (como imágenes y CSS)
+// Configurar express para servir archivos estáticos (como imágenes y carpetas)
 app.use('/imagenes', express.static(path.join(__dirname, 'public', 'imagenes')));
 app.use('/GAMA_FAMILIAR', express.static(path.join(__dirname, 'GAMA_FAMILIAR')));
+app.use('/GAMA_MEDIA', express.static(path.join(__dirname, 'GAMA_MEDIA')));
 
 // Ruta para servir el archivo PHP de Gama Familiar
 app.get('/GAMA_FAMILIAR/GAMA_FAMI.php', (req, res) => {
@@ -42,41 +42,3 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}`);
 });
-=======
-const express = require('express');
-const path = require('path');
-const { exec } = require('child_process'); // Para ejecutar comandos del sistema
-const app = express();
-
-// Ruta para ejecutar y servir el archivo PHP
-app.get('/', (req, res) => {
-  // Comando para ejecutar el archivo PHP
-  exec('php ' + path.join(__dirname, 'inicio.php'), (err, stdout, stderr) => {
-    if (err) {
-      // Si hay un error, muestra el error
-      console.error('Error:', err);
-      return res.status(500).send('Error ejecutando el archivo PHP');
-    }
-    if (stderr) {
-      // Si hay errores en stderr, muestra esos errores
-      console.error('stderr:', stderr);
-      return res.status(500).send('Error en la ejecución de PHP');
-    }
-    // Si no hay errores, responde con la salida del archivo PHP
-    res.send(stdout);
-  });
-});
-
-// Puerto donde el servidor escuchará
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Servidor corriendo en puerto ${port}`);
-});
-
-app.get('/GAMA_FAMILIAR/GAMA_FAMI.php', (req, res) => {
-  res.sendFile(path.join(__dirname, 'GAMA_FAMILIAR', 'GAMA_FAMI.php'));
-});
-
-
-app.use('/GAMA_FAMILIAR', express.static(path.join(__dirname, 'GAMA_FAMILIAR')));
->>>>>>> 385b4c70925897719a0be7311cee9ece92ce7643
