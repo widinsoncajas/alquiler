0759c6a357ecf1a77527ee3a014bee9966f80db9
widinsoncajas <adriancajas2002@gmail.com>
2025-04-29 20:54:27 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-// Configurar Express para servir archivos estáticos (si los tienes en la carpeta "public/imagenes")
+// Servir archivos estáticos
 app.use('/imagenes', express.static(path.join(__dirname, 'public', 'imagenes')));
 
-// Definir las rutas de redirección a PHP
+// Redirigir rutas a PHP
 app.get('/', (req, res) => {
-  res.redirect('http://localhost:8080/inicio.php');  // Redirecciona a la página PHP en tu servidor local
+  res.redirect('http://localhost:8080/inicio.php');
 });
 
 app.get('/GAMA_FAMILIAR/GAMA_FAMI.php', (req, res) => {
@@ -18,7 +18,12 @@ app.get('/GAMA_MEDIA/GAMA_MEDIAA.php', (req, res) => {
   res.redirect('http://localhost:8080/GAMA_MEDIAA.php');
 });
 
-// Establecer el puerto para Express (3000 o cualquier otro puerto que desees)
+// Middleware para manejar errores 404
+app.use((req, res) => {
+  res.status(404).send('Página no encontrada');
+});
+
+// Establecer el puerto de escucha
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Servidor corriendo en puerto ${port}`);
